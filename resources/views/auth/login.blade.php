499602560a093af44ac3511ee6fbad5c4e8eebf3
apgamilla <gamillajayson.work@gmail.com>
2022-04-02 14:47:40 +0800

    Removed forgot pass

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,6 @@
                     <p class="text-gray-600">Don't have an account? <a href="{{route('register')}}"
                                                                        class="font-bold">Sign
                             up</a>.</p>
-                    <p><a class="font-bold" href="{{ route('forget-password') }}">Forgot password?</a>.</p>
                 </div>
             </div>
         </div>
