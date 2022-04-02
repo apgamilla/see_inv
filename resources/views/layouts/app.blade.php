a1fd73544e31d108688c383b1181f494c563f5d0
apgamilla <gamillajayson.work@gmail.com>
2022-04-02 16:49:36 +0800

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anazel Website</title>
-    <link rel="shortcut icon" href="https://anazel.herokuapp.com/public/assetsassets/images/favicon.png">
+    <link rel="shortcut icon" href="https://anazel.herokuapp.com/public/assets/images/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://anazel.herokuapp.com/public/assets/css/bootstrap.css">
     <link rel="stylesheet" href="https://anazel.herokuapp.com/public/assets/vendors/bootstrap-icons/bootstrap-icons.css">
