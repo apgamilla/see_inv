70b49723e6ef8aa8d0778026abf2a7f992aa6f17
jayson gamilla <gamillajayson.work@gmail.com>
2022-04-02 09:52:35 +0800

    App host

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/anazel_'),
+    'url' => env('APP_URL', 'https://anazel.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
